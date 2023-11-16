ed24f112199157aec4d50911bf64ee1c82231110
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-11-16 12:54:13 -0300

    feat: componentizing some code

---
+++ src/Components/PagePreset/index.jsx
@@ -0,0 +1,34 @@
+import { Container, Grid, Paper } from "@mui/material"
+import { PageTitle } from "../PageTitle"
+import { ResponsiveHeader } from "../ResponsiveHeader"
+
+export const PagePreset = ({pageTitle}) => {
+    return (
+        <>
+        <ResponsiveHeader />
+        <PageTitle pageTitle={pageTitle} />
+        <Paper
+        elevation={3}
+        style={{
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: "1rem",
+          }}
+        >
+          <Grid
+            container
+            spacing={3}
+            style={{ margin: "1rem", marginLeft: "3rem" }}
+          ></Grid>
+          </Container>
+        </Paper>
+        </>
+    )
+}
--- src/Pages/Baby/index.jsx
+++ src/Pages/Baby/index.jsx
@@ -1,34 +1,7 @@
-import { Container, Grid, Paper } from "@mui/material"
-import { PageTitle } from "../../Components/PageTitle"
-import { ResponsiveHeader } from "../../Components/ResponsiveHeader"
+import { PagePreset } from "../../Components/PagePreset"
 
 export const Baby = () => {
     return (
-        <>
-        <ResponsiveHeader />
-        <PageTitle pageTitle="Baby" />
-        <Paper
-        elevation={3}
-        style={{
-          width: "100%",
-          height: "100vh",
-        }}
-      >
-        <Container
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            padding: "1rem",
-          }}
-        >
-          <Grid
-            container
-            spacing={3}
-            style={{ margin: "1rem", marginLeft: "3rem" }}
-          ></Grid>
-          </Container>
-        </Paper>
-        </>
+      <PagePreset pageTitle="Baby" />
     )
 }
