e237df7161c8800fa728a8a77fb6e313df46f3d0
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-11-17 18:06:47 -0300

    refactor: Refactoring queries to use in a postgres database

--- src/Server/SQL/tables.sql
+++ src/Server/SQL/tables.sql
@@ -1,30 +1,34 @@
+-- CREATE TABLE for 'users'
 CREATE TABLE IF NOT EXISTS users (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   relationship user_relationship NOT NULL
 );
 
+-- CREATE TABLE for 'baby'
 CREATE TABLE IF NOT EXISTS baby (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   age INT NOT NULL,
   weight DECIMAL(5, 2) NOT NULL,
   blood_type baby_blood_type NOT NULL
 );
 
+-- CREATE TABLE for 'breast_feeding'
 CREATE TABLE IF NOT EXISTS breast_feeding (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   baby_id INT NOT NULL,
-  time DATETIME NOT NULL,
+  time TIMESTAMP NOT NULL,
   side breast_feeding_side NOT NULL,
   hour DECIMAL(4, 2) NOT NULL,
   FOREIGN KEY (baby_id) REFERENCES baby(id)
 );
 
+-- CREATE TABLE for 'diapers'
 CREATE TABLE IF NOT EXISTS diapers (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   baby_id INT NOT NULL,
   label VARCHAR(50) NOT NULL,
   size diapers_size NOT NULL,
@@ -32,8 +36,9 @@ CREATE TABLE IF NOT EXISTS diapers (
   FOREIGN KEY (baby_id) REFERENCES baby(id)
 );
 
+-- CREATE TABLE for 'sleep'
 CREATE TABLE IF NOT EXISTS sleep (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   baby_id INT NOT NULL,
   date DATE NOT NULL,
   start_time TIME NOT NULL,
@@ -41,8 +46,9 @@ CREATE TABLE IF NOT EXISTS sleep (
   FOREIGN KEY (baby_id) REFERENCES baby(id)
 );
 
+-- CREATE TABLE for 'diary'
 CREATE TABLE IF NOT EXISTS diary (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   baby_id INT NOT NULL,
   date DATE NOT NULL,
   hour TIME NOT NULL,
