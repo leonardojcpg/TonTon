a7d2f86c1a69f04bc3966034957cb23c98ad7c90
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-11-20 18:54:54 -0300

    feat: update and delete queries from baby table

--- src/Server/Controllers/baby.controllers.js
+++ src/Server/Controllers/baby.controllers.js
@@ -1,4 +1,4 @@
-import { createBabyService, listBabyService } from "../Services/baby.services.js";
+import { createBabyService, deleteBabyService, listBabiesByIdService, listBabyService, updateBabyService } from "../Services/baby.services.js";
 
 export const createBabyController = async (req, res) => {
   try {
@@ -18,5 +18,39 @@ export const listBabyController = async (req, res) => {
     return res.status(200).json(babies);
   } catch (error) {
     return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+
+export const listBabiesByIdController = async (req, res) => {
+  const babyId = req.params.babyId;
+
+  try {
+    const baby = await listBabiesByIdService(babyId);
+
+    if (!baby) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    return res.status(200).json(baby);
+  } catch (error) {
+    console.error("Error fetching baby by ID:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const updateBabyController = async (req, res) => {
+  const baby = await updateBabyService(req.params.babyId, req.body);
+
+  return res.status(200).json(baby);
+};
+
+export const deleteBabyController = async (req, res) => {
+  try {
+    await deleteBabyService(req.params.babyId);
+    return res.status(204).send();
+  } catch (error) {
+    console.error('Error deleting baby:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 };
--- src/Server/Routes/baby.route.js
+++ src/Server/Routes/baby.route.js
@@ -1,17 +1,16 @@
 import { Router } from "express";
 import {
-  createBabyController, listBabyController,
+  createBabyController,
+  listBabiesByIdController,
+  listBabyController,
+  updateBabyController,
+  deleteBabyController,
 } from "../Controllers/baby.controllers.js";
-
 
 export const babyRoutes = Router();
 
 babyRoutes.post("/", createBabyController);
 babyRoutes.get("/", listBabyController);
-
-
-/* 
-babyRoutes.get("/:babyId", createBabyController);
-babyRoutes.patch("/:babyId", createBabyController);
-babyRoutes.delete("/:babyId", createBabyController); 
-*/
+babyRoutes.get("/:babyId", listBabiesByIdController);
+babyRoutes.patch("/:babyId", updateBabyController);
+babyRoutes.delete("/:babyId", deleteBabyController);
