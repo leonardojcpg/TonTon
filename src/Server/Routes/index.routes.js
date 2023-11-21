bc967a6ed32b0b41db10aa626753bfa8b6135e02
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-11-21 16:35:07 -0300

    feat: Creating sleep services, routes and controllers

--- src/Server/Routes/index.routes.js
+++ src/Server/Routes/index.routes.js
@@ -3,6 +3,7 @@ import { userRoutes } from "./user.route.js";
 import { babyRoutes } from "./baby.route.js";
 import { userBabyRoutes } from "./user_baby.routes.js";
 import { breastFeedingRoutes } from "./breastFeeding.routes.js";
+import { sleepRoutes } from "./sleep.routes.js";
 
 export const routes = Router()
 
@@ -10,3 +11,4 @@ routes.use("/users", userRoutes)
 routes.use("/baby", babyRoutes)
 routes.use("/user_baby", userBabyRoutes);
 routes.use("/breast_feeding", breastFeedingRoutes)
+routes.use("/sleep", sleepRoutes)
