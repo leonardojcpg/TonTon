38e5f507b459e0fd1734a10f453ca80e1d359a7e
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-11-21 21:14:39 -0300

    feat: verifyPermission middleware

---
+++ src/Server/Middlewares/verifyPermission.middlewares.js
@@ -0,0 +1,14 @@
+import { verify } from "jsonwebtoken"
+import AppError from "../Errors/App.error"
+
+export const verifyToken = (req, res, next) => {
+
+    const authorization = req.headers.authorization
+    if(!authorization){
+        throw new AppError("Missing JWT token", 401)
+    }
+    const token = authorization.split(" ")[1]
+    const decoded = verify(token, process.env.JWT_SECRET)
+    res.locals = {...res.locals, decoded}
+    return next()
+}
--- src/Server/Routes/user.route.js
+++ src/Server/Routes/user.route.js
@@ -8,13 +8,14 @@ import {
 } from "../Controllers/users.controllers.js";
 import { verifyEmail } from "../Middlewares/verifyEmail.middleware.js";
 import { verifyUserId } from "../Middlewares/verifyUserId.middlewares.js";
+import { verifyToken } from "../Middlewares/verifyPermission.middlewares.js";
 
 export const userRoutes = Router();
 
 userRoutes.get("/", listUsersController);
 userRoutes.post("/", verifyEmail, createUserController);
 
-userRoutes.use("/:userId", verifyUserId);
+userRoutes.use("/:userId", verifyToken, verifyUserId);
 userRoutes.get("/:userId", verifyUserId, listUsersByIdController);
 userRoutes.patch("/:userId", verifyEmail, updateUserController);
 userRoutes.delete("/:userId", deleteUserController);
